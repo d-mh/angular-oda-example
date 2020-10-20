950d1056f619d555973b34d425f2f1e858631c93
Dariusz Mach <mach.dariusz@gmail.com>
2020-10-20 12:54:57 +0200

    add partially working solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NgxStripeModule } from 'ngx-stripe';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    NgxStripeModule.forRoot('pk_test_51HbS6oCq3jNEslxH023W69OIi7G16fn2OjFF7H75KnTh6tjMUb1loKVJf7a35xzydMJ29sLnEBa3pXjixPIUJxFi00fzDrguzU'),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { StripeService } from 'ngx-stripe';
 import { ODAWebviewService } from '../shared/service/oda.webview.service';
+
+
+declare var stripe: any;
 
 interface OdaParams {
   userId: string;
@@ -21,6 +25,7 @@ export class PaymentComponent implements OnInit {
   constructor(
     private _route: ActivatedRoute,
     private _service: ODAWebviewService,
+    private _stripeService: StripeService,
   ) { }
 
   ngOnInit(): void {
@@ -68,4 +73,97 @@ export class PaymentComponent implements OnInit {
     );
   }
 
+  async checkout(): Promise<void> {
+
+    this._stripeService.redirectToCheckout({
+      sessionId: 'cs_test_Cvouahw25GsSO5VAeDphmdYR9ijmknEqdfMAGFGnlRweazlKfDcHwGjG',
+    }).subscribe(
+      success => { console.log('redirectToCheckout success', success); },
+      error => { console.error('redirectToCheckout error', error); },
+      () => console.log('redirectToCheckout completed'),
+    );
+
+    // const stripe = require('stripe')('sk_test_51HbS6oCq3jNEslxHoOcG6KqMnxdVnuXNM8CUtLJ1BuSmFhNnyHtWrCUxdvQqVW9q3aZxYNRbv75PIfu66CbDAWzd00m0D4xs2p');
+
+    // const session = await stripe.checkout.sessions.create({
+    //   payment_method_types: ['card'],
+    //   line_items: [
+    //     {
+    //       price_data: {
+    //         currency: 'usd',
+    //         product_data: {
+    //           name: 'T-shirt',
+    //         },
+    //         unit_amount: 2000,
+    //       },
+    //       quantity: 1,
+    //     },
+    //   ],
+    //   mode: 'payment',
+    //   success_url: 'https://example.com/success',
+    //   cancel_url: 'https://example.com/cancel',
+    // });
+
+    // this._stripeService.redirectToCheckout({
+    //   // lineItems: [{}]
+    //   // lineItems: [
+    //   //   { price: 'price_1HeGBPCq3jNEslxH8uCRZv4O', quantity: 1 },
+    //   // ],
+    //   mode: 'payment',
+    //   successUrl: 'https://f3da292b82d0.ngrok.io/stripe-success',
+    //   cancelUrl: 'https://f3da292b82d0.ngrok.io/stripe-failure',
+    // }).subscribe(
+    //   success => { console.log('redirectToCheckout success', success); },
+    //   error => { console.error('redirectToCheckout error', error); },
+    //   () => console.log('redirectToCheckout completed'),
+    // );
+
+    // lineItems: [
+    //   // Replace with the ID of your price
+    //   {price: 'price_123', quantity: 1},
+    // ],
+    // mode: 'payment',
+    // successUrl: 'https://your-website.com/success',
+    // cancelUrl: 'https://your-website.com/canceled',
+    // const session = await stripe.checkout.sessions.create({
+    //   success_url: 'https://6d7b71ba7aa2.ngrok.io/stripe-success',
+    //   cancel_url: 'https://6d7b71ba7aa2.ngrok.io/stripe-failure',
+    //   payment_method_types: ['card'],
+    //   mode: 'payment',
+    // });
+
+  }
+
 }
+
+/*
+
+<script type="text/javascript">
+  // Create an instance of the Stripe object with your publishable API key
+  var stripe = Stripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+  var checkoutButton = document.getElementById("checkout-button");
+
+  checkoutButton.addEventListener("click", function () {
+    fetch("/create-session", {
+      method: "POST",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (session) {
+        return stripe.redirectToCheckout({ sessionId: session.id });
+      })
+      .then(function (result) {
+        // If redirectToCheckout fails due to a browser or network
+        // error, you should display the localized error message to your
+        // customer using error.message.
+        if (result.error) {
+          alert(result.error.message);
+        }
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  });
+</script>
+*/
